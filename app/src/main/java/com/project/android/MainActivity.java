04c46d878fe92e6da5b275d6280778cbbe63759b
sherlock <1014952588@qq.com>
2018-09-03 20:37:33 +0800

    完成滑动菜单，实现退出登录

--- app/src/main/java/com/project/android/MainActivity.java
+++ app/src/main/java/com/project/android/MainActivity.java
@@ -1,15 +1,94 @@
 package com.project.android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        navigationView.setNavigationItemSelectedListener
+                (new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_quit:
+                        Intent intent = new Intent(MainActivity.this,
+                                LoginActivity.class);
+                        /*退出登录，返回至登录界面*/
+                        startActivity(intent);
+                        finish();
+                        break;
+                    default:
+                        /*其余无效情况，关闭滑动菜单*/
+                        mDrawerLayout.closeDrawers();
+                }
+                return true;
+            }
+        });
+    }
 
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     * <p>
+     * <p>Derived classes should call through to the base class for it to
+     * perform the default menu handling.</p>
+     *
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                /*打开左侧菜单*/
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.QR_code:
+                /*跳转到二维码扫描页面*/
+                Toast.makeText(MainActivity.this, "QR", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+        }
+        return true;
     }
 }
